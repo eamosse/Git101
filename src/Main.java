f8732d79a16d7947d13d3a70245d7d3080e7395a
Amosse Edouard <amosse.edouard@instant-system.com>
2025-11-26 20:44:43 +0100

    Explications des étapes Git

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Intiation à l'utilisation des commandes Git");
         System.out.println("Etape 1: commit - Enregister ses derniers changements");
         System.out.println("Etape 2: push -  Publiser ses changements");
+        System.out.println("Etape 3: pull - Récupérer les changements du serveur");
+        System.out.println("Etape 4: merge - Fusionner des changements du serveur avec le local");
     }
 }
