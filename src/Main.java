fe5fd5669c060ebfb82585d0387ebb60298000d5
whaugutin <whitchyaugustin@gmail.com>
2025-11-26 14:40:23 -0500

    Commit of "Hello World"

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Intiation à l'utilisation des commandes Git");
         System.out.println("Etape 1: commit - Enregister ses derniers changements");
         System.out.println("Etape 2: push -  Publiser ses changements");
-        System.out.println("Hello World");
+        System.out.println("Hello World of Whitchy AUGUSTIN");
     }
 }
